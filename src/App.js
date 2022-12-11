a14666b0d36e1d85294df3b8ca029a6150a400de
evrouge <evinlerouge@Evins-MacBook-Air.local>
2022-12-11 11:28:52 -0700

    udpated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-
 import './App.css';
+
+
+
+
+const App = () => {
+
+
+
+
+
+  return (
+    <>
+      <h1>book tracker</h1>
+      <h2>books</h2>
+    </>
+  )
+
+}
+
+
+
+
+
 
 
 export default App;
