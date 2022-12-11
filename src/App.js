8c19f5237ee128323d02f772fbe1748d9e8d73fe
evrouge <evinlerouge@Evins-MacBook-Air.local>
2022-12-11 12:08:40 -0700

    front end work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import './App.css';
-
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 const App = () => {
+  //use state
+  const [bookTitle, setBookTitle] = useState([]);
+  const [bookAuthor, setBookAuthor] = useState([]);
+  const [bookGenre, setBookGenre] = useState([]);
+  const [bookImage, setBookImage] = useState([]);
+  const [bookRead, setBookRead] = useState(false);
+  const [books, setBooks] = useState([]);
 
 
 
 
+  const newBookFormSubmit = (event) => {
+    event.preventDefault();
+    axios.post(
+      'https://papaya-mochi-57fe5a.netlify.app', {
+      title: bookTitle,
+      author: bookAuthor,
+      genre: bookGenre,
+      image: bookImage,
+      read: bookRead
+    }
+    ).then(() => {
+      axios.get('https://papaya-mochi-57fe5a.netlify.app').then((response) => {
+        setBooks(response.data)
+      })
+    })
+  }
+
+  //use effect
+  useEffect(() => {
+    axios.get('https://papaya-mochi-57fe5a.netlify.app').then((response) => {
+      setBooks(response.data)
+    })
+  }, [])
 
   return (
     <>
       <h1>book tracker</h1>
-      <h2>books</h2>
+      <h2>your books</h2>
+      <form onSubmit={newBookFormSubmit}>
+        Title: <input type="text" />
+        <input type="submit" value="create book" />
+      </form>
     </>
   )
 
