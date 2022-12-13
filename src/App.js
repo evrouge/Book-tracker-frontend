71b819555a43f3b92e8d717ad5e7d29fdacce6f7
evrouge <evinlerouge@Evins-MacBook-Air.local>
2022-12-13 16:29:43 -0700

    everything working now

--- src/App.js
+++ src/App.js
@@ -1,27 +1,204 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Book from './components/bookapp.js';
 
-
-
-
+//===========================================================================
+//============================= Hooks =======================================
 const App = () => {
+  //hook for title
+  const [bookTitle, setBookTitle] = useState('');
+  //hook for author
+  const [bookAuthor, setBookAuthor] = useState('');
+  //hook for genre
+  const [bookGenre, setBookGenre] = useState('');
+  //hook for image
+  const [bookImage, setBookImage] = useState('');
+  //hook for if the book has been read / default false
+  const [bookRead, setBookRead] = useState(false);
+  //another hook
+  const [books, setBooks] = useState([]);
+
+  //=====================================================================
+  //=========================== Hooks for updating ======================
+  //update title hook
+  const [updateTitle, setUpdateTitle] = useState();
+  //update author hook
+  const [updateAuthor, setUpdateAuthor] = useState();
+  //update genre hook
+  const [updateGenre, setUpdateGenre] = useState();
+  //update image hook
+  const [updateImage, setUpdateImage] = useState();
+  //update read/checked hook
+  const [updateRead, setUpdateRead] = useState();
 
 
 
+  //title change function
+  const newTitleChange = (event) => {
+    setBookTitle(event.target.value)
+  }
+
+  //author change function
+  const newAuthorChange = (event) => {
+    setBookAuthor(event.target.value)
+  }
+
+  //genre change function
+  const newGenreChange = (event) => {
+    setBookGenre(event.target.value)
+  }
+
+  //image change function
+  const newImageChange = (event) => {
+    setBookImage(event.target.value)
+  }
+
+  //read or not checkbox function
+  const newReadOrNotChange = (event) => {
+    setBookRead(event.target.checked)
+  }
+
+  //=========================================================================
+  //========================= Update functions ==============================
+  // update title function
+  const handleUpdateTitle = (event) => {
+    setUpdateTitle(event.target.value);
+  }
+
+  // update author function
+  const handleUpdateAuthor = (event) => {
+    setUpdateAuthor(event.target.value);
+  }
+
+  // update genre function
+  const handleUpdateGenre = (event) => {
+    setUpdateGenre(event.target.value)
+  }
+
+  // update image function
+  const handleUpdateImage = (event) => {
+    setUpdateImage(event.target.value)
+  }
+
+  // update read checkbox function
+  const handleUpdateRead = (event) => {
+    setUpdateRead(event.target.checked)
+  }
+
+  //===============================================================================
+  //================================ Functions ====================================
+  //submit form function
+  const newBookFormSubmit = (event) => {
+    event.preventDefault();
+    axios.post(
+      'https://murmuring-citadel-25803.herokuapp.com/books', {
+
+      title: bookTitle,
+      author: bookAuthor,
+      genre: bookGenre,
+      image: bookImage,
+      read: bookRead
+    }
+    ).then(() => {
+      axios.get('https://murmuring-citadel-25803.herokuapp.com/books').then((response) => {
+
+        setBooks(response.data)
+      })
+    })
+  }
 
 
+  //=====================================================================
+  //================================= Delete ============================
+  const handleDelete = (bookData) => {
+    axios.delete(`https://murmuring-citadel-25803.herokuapp.com/books/${bookData._id}`)
+      .then(() => {
+        axios.get('https://murmuring-citadel-25803.herokuapp.com/books').then((response) => {
+          setBooks(response.data)
+        })
+      })
+  }
+
+  //===================================================================
+  //======================== Submit for Update ===========================
+  const updateSubmit = (bookData) => {
+
+
+    axios.put(`https://murmuring-citadel-25803.herokuapp.com/books/${bookData._id}/edit`,
+      {
+        title: updateTitle,
+        author: updateAuthor,
+        genre: updateGenre,
+        image: updateImage,
+        read: updateRead
+      }
+    ).then(() => {
+      axios.get('https://murmuring-citadel-25803.herokuapp.com/books').then((response) => {
+        console.log(response.data)
+        setBooks(response.data)
+        console.log(bookData)
+      })
+    })
+  }
+
+
+  //====================================================================
+  //==================== Use Effect ====================================
+  useEffect(() => {
+    axios.get('https://murmuring-citadel-25803.herokuapp.com/books').then((response) => {
+      setBooks(response.data)
+    })
+  }, [])
+
+  //=========================================================================
+  //=========================================================================
   return (
     <>
-      <h1>book tracker</h1>
-      <h2>books</h2>
+      <h1>Book Tracker App</h1>
+      <h2>Add A New Book</h2>
+      <section>
+        {/* Create book section */}
+        <form onSubmit={newBookFormSubmit}>
+          Title: <input type="text" onChange={newTitleChange} /><br />
+          Author: <input type="text" onChange={newAuthorChange} /><br />
+          Genre: <input type="text" onChange={newGenreChange} /><br />
+          Image URL: <input type="text" onChange={newImageChange} /><br />
+          Have you read this book? <input type="checkbox" onChange={newReadOrNotChange} /> Yes<br />
+          <input type="submit" value="Submit Book" />
+        </form>
+      </section>
+      {/* Show books section */}
+      <section>
+        <h3>Books you have not read yet:</h3>
+        {
+
+
+
+          books.map((book, i) => {
+
+            return (
+              <div key={i}>
+
+                <Book key={i} book={book} updateSubmit={updateSubmit}
+                  handleUpdateTitle={handleUpdateTitle}
+                  handleUpdateAuthor={handleUpdateAuthor}
+                  handleUpdateGenre={handleUpdateGenre}
+                  handleUpdateImage={handleUpdateImage}
+                  handleUpdateRead={handleUpdateRead}
+                  handleDelete={handleDelete}
+                />
+              </div>
+            )
+          })
+        }
+        <h3>Books you have read:</h3>
+
+      </section>
     </>
   )
-
 }
 
-
-
-
-
-
-
+//================================================================================
+//================================================================================
 export default App;
---
+++ src/components/bookapp.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import '../App.css';
+//==========================================================
+//================== Props =================================
+const Book = (props) => {
+
+    //=====================================================
+    //================ Edit Hook ============================
+    const [edits, setEdits] = useState(false);
+
+    //========================================================
+    //================= Edit Function ========================
+    const handleEdit = () => {
+        setEdits(!edits);
+    }
+
+    //=============================================================
+    //==============================================================
+
+    return (
+        <>
+
+            {/* {(props.book.read) ? */}
+            <div>
+
+                <img src={props.book.image} /> <br />
+                <h3>{props.book.title}</h3>
+                <h3>{props.book.author}</h3>
+                <h3>{props.book.genre}</h3>
+                <h3>{props.book.read}</h3>
+                <button onClick={(event) => { props.handleDelete(props.book) }}>Delete</button>
+                <button onClick={handleEdit}>Edit</button>
+
+                {edits ?
+                    <div>
+                        <h2>Edit Book</h2>
+                        <form onSubmit={(event) => { event.preventDefault(); props.updateSubmit(props.book) }}>
+                            Title: <input type="text" defaultValue={props.book.title} onChange={props.handleUpdateTitle} /><br />
+                            Author: <input type="text" defaultValue={props.book.author} onChange={props.handleUpdateAuthor} /><br />
+                            Genre: <input type="text" defaultValue={props.book.genre} onChange={props.handleUpdateGenre} /><br />
+                            Image: <input type="text" defaultValue={props.book.image} onChange={props.handleUpdateImage} /><br />
+                            Have you read this book?<input type="checkbox" defaultChecked={props.book.read} onChange={props.handleUpdateRead} /><br />
+                            <input type="submit" value="Update Book" />
+                        </form>
+                    </div>
+
+                    : null}
+            </div>
+            {/* : null
+            } */}
+
+        </>
+    )
+}
+
+//========================================================
+//========================================================
+export default Book;
