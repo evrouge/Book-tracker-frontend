190c17439f771b07dbc0035457583c00e9cf0d12
evrouge <evinlerouge@Evins-MacBook-Air.local>
2022-12-13 13:22:46 -0700

    asdfad

---
+++ src/components/bookapp.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import '../App.css';
+//==========================================================
+//================== Props =================================
+const Book = (props) => {
+
+    //=====================================================
+    //================ Edit Hook ============================
+    const [edits, setEdits] = useState(false);
+
+    //========================================================
+    //================= Edit Function ========================
+    const handleEdit = () => {
+        setEdits(!edits);
+    }
+
+    return (
+        <>
+            {
+                (props.book.read) ?
+                    <>
+                        <h3>{props.book.title}</h3><br />
+                        <h3>{props.book.author}</h3><br />
+                        <h3>{props.book.genre}</h3><br />
+                        <img src={props.book.image} /> <br />
+                        <h3>{props.book.read}</h3><br />
+                        <button onClick={(event) => { props.handleNewDelete(props.book) }} >Delete</button>
+                        <button onClick={handleEdit}>Edit</button>
+
+                        {
+                            edits ?
+                                <div>
+                                    <h2>Edit Book</h2>
+                                    <form onSubmit={(event) => { props.updateSubmit(props.book) }}>
+                                        Title: <input type="text" defaultValue={props.book.title} onChange={props.handleUpdateTitle} />
+                                        Author: <input type="text" defaultValue={props.book.author} onChange={props.handleUpdateAuthor} />
+                                        Genre: <input type="text" defaultValue={props.book.genre} onChange={props.handleUpdateGenre} />
+                                        Image: <input type="text" defaultValue={props.book.image} onChange={props.handleUpdateImage} />
+                                        Have you read this book? <input type="checkbox" defaultChecked={props.book.read} onChange={props.handleUpdateRead} />
+                                        <input type="submit" value="Update Book" />
+                                    </form>
+                                </div>
+                                :
+                                null}</>
+                    :
+                    <></>
+            }
+
+        </>
+    )
+}
+
+//========================================================
+//========================================================
+export default Book;
